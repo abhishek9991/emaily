e2bb8e33e5b303d53481212674a345a0a751aa81
Abhishek yadav <abhishek@abhi-ubuntu.local>
2020-10-03 01:05:21 +0530

    fixed typo

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
     //we are in production environment
     module.exports = require('./prod');
-} eles {
+} else {
     //we are in development environment
     module.exports = require('./dev');
 }
