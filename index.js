88133d6bebae7e5a04a65119af54fbae556309d7
Abhishek yadav <abhishek@abhi-ubuntu.local>
2020-10-13 00:57:37 +0530

    Added billing and client side app

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/Users');
 require('./services/passport');
+const bodyParser = require('body-parser');
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
     cookieSession({
@@ -17,10 +20,25 @@ app.use(
     })
 );
 
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.send.NODE_ENV === 'production') {
+    // express will serve up production assets
+    //like our main.js file, or main.css file!
+    app.use(express.static('client/build'));
+
+    //express will serve up index.html file
+    //if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
